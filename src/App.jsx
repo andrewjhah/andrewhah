c7b6e1d6ab2552fcd91df7f5bf9cab9d0f6cee08
Andrew Hah <andrew.hah@gmail.com>
2023-03-07 00:14:13 -0500

    03/06/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Contact from "./components/Contact"
+import styled from "styled-components"
+
+const Container = styled.div`
+  height: 100vh;
+  background-color: blue;
+
+`
+
 function App() {
 
   return (
-    <div>
-      Hello world!
-    </div>
+    <Container>
+      <Hero/>
+      <About/>
+      <Projects/>
+      <Contact/>
+    </Container>
   )
 }
 
